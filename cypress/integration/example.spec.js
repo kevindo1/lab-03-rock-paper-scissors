d64f9a8443ead637c23fe053d75a666cdbcfb4a1
Kevin Do <kevind0501@gmail.com>
2021-10-06 16:40:25 -0700

    Added an additional cypress test

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -4,11 +4,17 @@ describe('Example Test', ()=>{
         cy.visit('/');
     });
 
-    describe('user-interaction', ()=>{   
+    describe('error message', ()=>{   
         it('should play RPS against computer', ()=>{
-            cy.get('#rock').click();
             cy.get('#submit-button').click();
-            cy.get('#win-message').should('be.visible');
+            cy.get('#error-message').should('be.visible');
+        });
+    });
+
+    describe('reset button', ()=>{   
+        it('should reset the game', ()=>{
+            cy.get('#reset-button').click();
+            cy.get('#reset-message').should('be.visible');
         });
     });
 });
