1f1c433dbdccd215702f78011457e6cdf6ac1796
Kevin Do <kevind0501@gmail.com>
2021-10-06 16:24:51 -0700

    Added in additional TDD tests

--- test/example.test.js
+++ test/example.test.js
@@ -5,14 +5,66 @@ import { doesUserWin } from '../rps.js';
 const test = QUnit.test;
 
 test('testing doesUserWin', (expect) => {
-    const userSelected = 'rock';
-    const getComputerSelected = 'scissors';
-    
-    const actual = doesUserWin(userSelected, getComputerSelected);
+    const actual = doesUserWin('rock', 'scissors');
     const expected = true;
 
     expect.equal(actual, expected, 'should be a win');
 });
 
+test('testing doesUserWin', (expect) => {
+    const actual = doesUserWin('rock', 'paper');
+    const expected = false;
+
+    expect.equal(actual, expected, 'should be a loss');
+});
+
+test('testing doesUserWin', (expect) => {
+    const actual = doesUserWin('paper', 'scissors');
+    const expected = false;
+
+    expect.equal(actual, expected, 'should be a loss');
+});
+
+test('testing doesUserWin', (expect) => {
+    const actual = doesUserWin('paper', 'rock');
+    const expected = true;
+
+    expect.equal(actual, expected, 'should be a win');
+});
+
+test('testing doesUserWin', (expect) => {
+    const actual = doesUserWin('scissors', 'rock');
+    const expected = false;
+
+    expect.equal(actual, expected, 'should be a loss');
+});
+
+test('testing doesUserWin', (expect) => {
+    const actual = doesUserWin('scissors', 'paper');
+    const expected = true;
+
+    expect.equal(actual, expected, 'should be a win');
+});
+
+test('testing doesUserWin', (expect) => {
+    const actual = doesUserWin('paper', 'paper');
+    const expected = 'draw';
+
+    expect.equal(actual, expected, 'should be a win');
+});
+
+test('testing doesUserWin', (expect) => {
+    const actual = doesUserWin('rock', 'rock');
+    const expected = 'draw';
+
+    expect.equal(actual, expected, 'should be a win');
+});
+
+test('testing doesUserWin', (expect) => {
+    const actual = doesUserWin('scissors', 'scissors');
+    const expected = 'draw';
+
+    expect.equal(actual, expected, 'should be a win');
+});
 
 
