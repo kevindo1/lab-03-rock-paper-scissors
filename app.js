99c13e44276082ffb3820c869b88b2b0441e09b7
Kevin Do <kevind0501@gmail.com>
2021-10-06 14:27:16 -0700

    added HTML CSS JS of rock paper scissors

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
+import { getComputerSelected, doesUserWin } from "./rps.js";
+
 // import functions and grab DOM elements
+const submitButton = document.getElementById('submit-button');
+const winMessage = document.getElementById('win-message');
+const loseMessage = document.getElementById('lose-message');
+const drawMessage = document.getElementById('draw-message');
+const errorMessage = document.getElementById('error-message');
 
 // initialize global state
+let win = 0;
+let lose = 0;
+let draw = 0;
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+submitButton.addEventListener('click', ()=> {
+    const radioSelected = document.querySelector('input[type=radio]:checked');
+
+    if (!radioSelected) {
+      return errorMessage.classList.remove('hidden-message');
+    }
+
+    const userSelected = radioSelected.value;
+    const computerSelected = getComputerSelected();
+
+    if (userSelected === computerSelected) {
+      draw++
+    } else if (doesUserWin(userSelected, computerSelected)) {
+      win++
+    } else {
+      lose++
+    }
+    
+    winMessage.textContent = win;
+    loseMessage.textContent = lose;
+    drawMessage.textContent = draw;
+});
+
---
+++ rps.js
@@ -0,0 +1,22 @@
+// export function didUserWin(userChoice, computerChoice) {
+//     if (userChoice)
+// };
+
+export function getComputerSelected() {
+    return ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)];
+};
+
+export function doesUserWin(userSelected, getComputerSelected) {
+    if (userSelected === getComputerSelected) {
+        return 'draw'
+    }
+    if (userSelected === 'rock' && getComputerSelected === 'scissors') {
+        return true;
+    } else if (userSelected === 'scissors' && getComputerSelected === 'paper') {
+        return true;
+    } else if (userSelected === 'paper' && getComputerSelected === 'rock') {
+        return true;
+    } else {
+        return false;
+    }
+};
